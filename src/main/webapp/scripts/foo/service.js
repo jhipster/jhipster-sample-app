ec4d15cd516db8dda25f6a728ee254bb96b5dc48
Jérôme Mirc <jerome.mirc@gmail.com>
2014-03-16 22:30:01 -0400

    Remove foo references

--- src/main/webapp/scripts/foo/service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-jhipsterApp.factory('Foo', ['$resource',
-    function ($resource) {
-        return $resource('app/rest/foos/:id', {}, {
-            'query': { method: 'GET', isArray: true},
-            'get': { method: 'GET'}
-        });
-    }]);
