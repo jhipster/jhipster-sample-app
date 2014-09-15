959a3007c57d012c108890f4c502ece5c0737898
Julien Dubois <julien.dubois@gmail.com>
2014-09-15 18:30:03 +0200

    Revert "Corrected language issue mentioned in #573"
    This reverts commit 4938efd006a7c04911e44d9c9a647a174bd6fd68.

--- src/main/webapp/scripts/services.js
+++ src/main/webapp/scripts/services.js
@@ -8,9 +8,6 @@ jhipsterApp.factory('LanguageService', function ($http, $translate, LANGUAGES) {
                 if (language == undefined) {
                     language = $translate.storage().get('NG_TRANSLATE_LANG_KEY');
                 }
-                if (language == undefined) {
-                    language = 'en';
-                }
 
                 var promise =  $http.get('/i18n/' + language + '.json').then(function(response) {
                     return LANGUAGES;
